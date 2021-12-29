703d86ef9d4a47b6a58ff5967f016842ab63252b
kakkki <kakki4062@gmail.com>
2021-12-29 15:51:49 +0900

    Initial Commit

---
+++ swiftui-dictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-dictionary
+//
+//  Created by Atsuki Kakehi on 2021/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-dictionary/swiftui_dictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_dictionaryApp.swift
+//  swiftui-dictionary
+//
+//  Created by Atsuki Kakehi on 2021/12/29.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_dictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
