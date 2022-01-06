31d8e6ecf6dd1db0135e088839fd3bc6e4c6b3e3
筧敦気 <atsuki@kakeiatsushikinoiMac.local>
2022-01-06 09:46:21 +0900

    intro Design Code Sample FeaturedItem

---
+++ swiftui-dictionary/design-code-samples/Views/FeaturedItem.swift
@@ -0,0 +1,63 @@
+//
+//  FeaturedItem.swift
+//  swiftui-dictionary
+//
+//  Created by 筧敦気 on 2022/01/06.
+//
+
+import SwiftUI
+
+struct FeaturedItem: View {
+    var course: Course = sampleCourses[0]
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image(course.logo)
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(10)
+                .padding(9)
+                .background(
+                    .ultraThinMaterial,
+                    in: RoundedRectangle(cornerRadius: 16, style: .continuous)
+                )
+            Text(course.title)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .lineLimit(1)
+            Text(course.subtitle.uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+            Text(course.text)
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
+        }
+        .padding(.all, 20.0)
+        .padding(.vertical, 20)
+        .frame(height: 350.0)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding(.horizontal, 20)
+        .overlay(
+            Image(course.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
+            
+        )
+    }
+}
+
+struct FeaturedItem_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedItem()
+    }
+}
