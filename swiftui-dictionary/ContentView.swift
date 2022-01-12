054806bca9e82096f6492cf4f795acca02c110b5
kakkki <kakki4062@gmail.com>
2022-01-12 15:40:05 +0900

    modify ContentView

--- swiftui-dictionary/ContentView.swift
+++ swiftui-dictionary/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            List(samples) { sample in
-                NavigationLink(sample.title, destination: sample.desinationView)
+        VStack {
+            NavigationView {
+                List(samples) { sample in
+                    NavigationLink(sample.title, destination: sample.desinationView)
+                }
             }
         }
     }
@@ -20,24 +22,33 @@ struct ContentView: View {
 struct Sample: Identifiable {
     let id = UUID()
     let title: String
-    let desinationView: AnyView
+    let desinationView: AnyView?
 }
 
 let samples:[Sample] = [
     Sample(title: "DashedLineSample", desinationView: AnyView(DashedLineSample())),
+    Sample(title: "--------------------------------------", desinationView: nil),
     Sample(title: "SyncColumnWidthSample", desinationView: AnyView(SyncColumnWidthSample())),
     Sample(title: "GeometryPreferenceSample", desinationView: AnyView(GeometryPreferenceSample())),
     Sample(title: "AnchorPreferenceSample", desinationView: AnyView(AnchorPreferenceSample())),
     Sample(title: "NestedViewPreferenceSample", desinationView: AnyView(NestedViewPreferenceSample())),
     Sample(title: "SetPositionToCenterExample", desinationView: AnyView(SetPositionToCenterExample())),
     Sample(title: "PositionChangeExample", desinationView: AnyView(PositionChangeExample())),
+    Sample(title: "--------------------------------------", desinationView: nil),
     Sample(title: "TrackableScrollViewSample todo implement", desinationView: AnyView(TrackableScrollViewSample())),
+    Sample(title: "--------------------------------------", desinationView: nil),
     Sample(title: "FeaturedItem", desinationView: AnyView(FeaturedItem())),
     Sample(title: "TabbedFeaturedItems", desinationView: AnyView(TabbedFeaturedItems())),
     Sample(title: "Rotation3DEffectTab", desinationView: AnyView(Rotation3DEffectTab())),
+    Sample(title: "--------------------------------------", desinationView: nil),
     Sample(title: "DragGesture Basic", desinationView: AnyView(DragGestureBasic())),
     Sample(title: "DragGestureAutoPositionChange", desinationView: AnyView(DragGestureAutoPositionChange())),
-    Sample(title: "Replace Animation API from iOS15", desinationView: AnyView(AnimationUpdateSample()))
+    Sample(title: "--------------------------------------", desinationView: nil),
+    Sample(title: "Replace Animation API from iOS15", desinationView: AnyView(AnimationUpdateSample())),
+    Sample(title: "LeaningCardSample", desinationView: AnyView(LeaningCardSample())),
+    Sample(title: "StackLayoutSample", desinationView: AnyView(StackLayoutSample())),
+    Sample(title: "LeaningStackSample", desinationView: AnyView(LeaningStackSample())),
+    Sample(title: "StackCardsAnimationSample", desinationView: AnyView(StackCardsAnimationSample()))
 
 ]
 
--- swiftui-dictionary/StackLayoutCards/StackCardsAnimationSample.swift
+++ swiftui-dictionary/StackLayoutCards/StackCardsAnimationSample.swift
@@ -1,5 +1,5 @@
 //
-//  StackCardsAnimation.swift
+//  StackCardsAnimationSample.swift
 //  swiftui-dictionary
 //
 //  Created by Atsuki Kakehi on 2022/01/12.
@@ -7,14 +7,97 @@
 
 import SwiftUI
 
-struct StackCardsAnimation: View {
+struct StackCardsAnimationSample: View {
+    @State var show = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            TitleView()
+                .blur(radius: show ? 6 : 0)
+                .animation(.default, value: show)
+
+            Spacer()
+                .frame(width: 340, height: 220)
+                .background(Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400 : -40)
+                .scaleEffect(0.9)
+                .rotationEffect(.degrees(show ? 0 : 10))
+                .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5), value: show)
+
+
+            Spacer()
+                .frame(width: 340, height: 220)
+                .background(Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .scaleEffect(0.95)
+                .rotationEffect(Angle.degrees(show ? 0 : 5))
+                .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5), value: show)
+
+            FrontCardView()
+                .blendMode(.hardLight)
+                .onTapGesture {
+                    show.toggle()
+                }
+        }
     }
 }
 
-struct StackCardsAnimation_Previews: PreviewProvider {
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct FrontCardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 20)
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct StackCardsAnimationSample_Previews: PreviewProvider {
     static var previews: some View {
-        StackCardsAnimation()
+        StackCardsAnimationSample()
     }
 }
